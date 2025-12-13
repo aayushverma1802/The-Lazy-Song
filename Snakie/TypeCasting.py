2450cd2448414d528b98d1ba91cf64084a12ff37
aayushverma1802 <aayush.verma2021@vitstudent.ac.in>
2025-12-13 12:25:04 +0530

    Typecasting

--- Snakie/TypeCasting.py
+++ Snakie/TypeCasting.py
@@ -1,11 +1,13 @@
 a="1"
 b="2"
 print(a+b)
-
+# Explicit Typecasting (Consversion is done by the person)
 a="1"
 b="2"
 print(int(a)+int(b))
-
-
+#Implicit Typecasting (Conversion is done by python on its own)
+f1=8
+f2=1.2
+print(f1+f2)
 
 
