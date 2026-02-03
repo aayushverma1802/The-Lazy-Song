328b910fee9a7f9a475a4c6bf20df4b3c5435ddb
aayushverma1802 <aayush.verma2021@vitstudent.ac.in>
2026-02-03 22:36:15 +0530

    124

---
+++ BrahMos/124_Binary_Tree_Maximum_Path_Sum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    ans=float('-inf')
+    def Height(self, root: Optional[TreeNode])-> int:
+        if root==None:
+            return 0
+        l=self.Height(root.left)
+        if l<0:
+            l=0
+        r=self.Height(root.right)
+        if r<0:
+            r=0
+        self.ans=max(self.ans,l+r+root.val)
+        return max(l,r)+root.val
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        if root==None:
+            return 0
+        self.Height(root)
+        return self.ans
