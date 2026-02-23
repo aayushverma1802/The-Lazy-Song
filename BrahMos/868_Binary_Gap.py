b9855fecfd9ec0e642a5d26b6e2cc59e74eb13d0
aayushverma1802 <aayush.verma2021@vitstudent.ac.in>
2026-02-23 23:25:24 +0530

    Optimized

--- BrahMos/868_Binary_Gap.py
+++ BrahMos/868_Binary_Gap.py
@@ -13,4 +13,18 @@ class Solution:
                     j+=1
 
         return ans
-  
+# Optimized
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        int_num=bin(n)[2:]
+        ans=0
+        prev=-1
+        for i,num in enumerate(int_num):
+            if num=='1':
+                if prev!=-1:
+                    ans=max(ans,i-prev)
+                prev=i
+
+
+
+        return ans
